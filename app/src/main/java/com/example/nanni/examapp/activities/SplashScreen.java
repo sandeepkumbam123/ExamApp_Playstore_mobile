2c19b781dfd092463ec316bcf90a7b9ca7dbeb54
Poojitha Aare <kumbhamsandeep123@gmail.com>
2017-07-02 19:35:35 +0530

    Designs are done

---
+++ app/src/main/java/com/example/nanni/examapp/activities/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.example.nanni.examapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nanni.examapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
